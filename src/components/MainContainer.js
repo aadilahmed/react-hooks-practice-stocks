515edc4d7da61fb2fbaa5b5d8d77c501ae29d8fd
Aadil Ahmed <aadilahmed0@gmail.com>
2023-06-12 00:41:47 -0700

    complete lab

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([]);
+  const [portfolioStocks, setPortfolioStocks] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("Tech");
+  const [isChecked, setChecked] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+      .then((response) => response.json())
+      .then((data) => setStocks(data));
+  }, []);
+
+  function handleAddToPortfolio(newStock) {
+    setPortfolioStocks([...portfolioStocks, newStock]);
+  }
+
+  function handleDeleteFromPortfolio(deletedStock) {
+    const updatedPortfolio = portfolioStocks.filter(
+      (stock) => stock.id !== deletedStock.id
+    );
+    setPortfolioStocks(updatedPortfolio);
+  }
+
+  function handleCategoryChange(category) {
+    setSelectedCategory(category);
+  }
+
+  function handleSortChange(sortBy) {
+    setChecked(sortBy);
+  }
+
+  const stocksToDisplay = stocks.filter((stock) => {
+    return stock.type === selectedCategory;
+  });
+
+  if (isChecked === "Alphabetically") {
+    stocksToDisplay.sort((a, b) =>
+      a.ticker > b.ticker ? 1 : b.ticker > a.ticker ? -1 : 0
+    );
+  } else if (isChecked === "Price") {
+    stocksToDisplay.sort((a, b) =>
+      a.price > b.price ? 1 : b.price > a.price ? -1 : 0
+    );
+  }
+
   return (
     <div>
-      <SearchBar />
+      <SearchBar
+        category={selectedCategory}
+        onCategoryChange={handleCategoryChange}
+        isChecked={isChecked}
+        onSortChange={handleSortChange}
+      />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer
+            stocks={stocksToDisplay}
+            onAddStock={handleAddToPortfolio}
+          />
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer
+            stocks={portfolioStocks}
+            onDeleteStock={handleDeleteFromPortfolio}
+          />
         </div>
       </div>
     </div>
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function SearchBar() {
+function SearchBar({ category, onCategoryChange, isChecked, onSortChange }) {
   return (
     <div>
       <strong>Sort by:</strong>
@@ -9,8 +9,8 @@ function SearchBar() {
           type="radio"
           value="Alphabetically"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={isChecked === "Alphabetically"}
+          onChange={(e) => onSortChange(e.target.value)}
         />
         Alphabetically
       </label>
@@ -19,15 +19,18 @@ function SearchBar() {
           type="radio"
           value="Price"
           name="sort"
-          checked={null}
-          onChange={null}
+          checked={isChecked === "Price"}
+          onChange={(e) => onSortChange(e.target.value)}
         />
         Price
       </label>
       <br />
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select
+          value={category}
+          onChange={(e) => onCategoryChange(e.target.value)}
+        >
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
